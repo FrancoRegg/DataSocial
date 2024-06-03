5bfb475050f66e1d42eaa571963709523e7f6349
FrancoRegg <francoreggiardo97@gmail.com>
2024-06-03 11:41:46 +0200

    Actualizo puerto

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, Flask!"
+  return "Hello, Flask!"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+  PORT = int(os.environ.get('PORT', 3001))
+  app.run(host='0.0.0.0', port=PORT, debug=True)
