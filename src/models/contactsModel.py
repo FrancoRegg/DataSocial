8d166795666fab6bf56dd73ada9d05c7b631ffbb
FrancoRegg <francoreggiardo97@gmail.com>
2024-06-03 11:42:17 +0200

    Elimino columnas que no utilizo

--- src/models/contactsModel.py
+++ src/models/contactsModel.py
@@ -8,9 +8,6 @@ class Contacts(db.Model):
   id = db.Column(db.Integer, primary_key=True)
   name = db.Column(db.String(60), unique=False, nullable=False)
   email = db.Column(db.String(120), unique=True, nullable=False)
-  phone_number = db.Column(db.String(20), nullable=False)
-  create_date = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
-  update_date = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow, nullable=False)
   
   def __repr__(self):
     return '<Contacts %r>' % self.id
@@ -19,8 +16,4 @@ class Contacts(db.Model):
     return {
       "id": self.id,
       "name":self.name,
-      "email": self.email,
-      "phone_number": self.phone_number,
-      "create_date": self.create_date.isoformat(),
-      "update_date": self.update_date.isoformat()
-    } 
+      } 
