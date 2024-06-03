9638af87a38edf4fca9a9964882c856fc3de0899
FrancoRegg <francoreggiardo97@gmail.com>
2024-06-03 10:09:25 +0200

    Primera tabla de contactos

---
+++ src/models/contactsModel.py
@@ -0,0 +1,27 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+db = SQLAlchemy()
+
+class Contacts(db.Model):
+  __tablename__ = 'contacts'
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String(60), unique=False, nullable=False)
+  email = db.Column(db.String(120), unique=True, nullable=False)
+  phone_number = db.Column(db.String(20), nullable=False)
+  create_date = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
+  update_date = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow, nullable=False)
+  
+  def __repr__(self):
+    return '<Contacts %r>' % self.id
+
+  def serialize(self):
+    return {
+      "id": self.id,
+      "name":self.name,
+      "email": self.email,
+      "phone_number": self.phone_number,
+      "create_date": self.create_date.isoformat(),
+      "update_date": self.update_date.isoformat()
+    } 
