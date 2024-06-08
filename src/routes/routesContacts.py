716656c3554abc36e6c2d81aa6d70ddb222a722b
FrancoRegg <francoreggiardo97@gmail.com>
2024-06-08 18:43:13 +0200

    Importo funciones para trabajar con hubspot y creo la ruta para trabajar con webhook

--- src/routes/routesContacts.py
+++ src/routes/routesContacts.py
@@ -1,9 +1,10 @@
 from flask import  Flask, request, jsonify
 from models import db, contactsModel
+from utils import upsert_contact, update_hubspot
 from . import app
 
-
-@app.route('/contacts', methods=['GET','POST'])
+'''
+@app.route('/contacts', methods=['POST'])
 def create_or_update_contact():
   #Extrae datos del cuero de la solicitud 
   body = request.get_json(silent=True)
@@ -19,9 +20,19 @@ def create_or_update_contact():
   db.session.commit()
   #update_hubspot(email, name)
   return jsonify('OK'), 200
-
+'''
 @app.route('/webhook', method=['POST'])
 def webhook():
   body = request.get_json(silent=True)
-  print("ACA Esta body",body)
-  return jsonify("OK"), 200
+  email = body.get('email')
+  name = body.get('name')
+
+  if not email:
+    return jsonify("Email is required"), 400
+  if not name:
+    return jsonify("Name is required"), 400
+  
+  contact = upsert_contact(email, name)
+  update_hubspot(email, name)
+
+  return jsonify({"message": "Contact processed successfully", "contact": {"email": contact.email, "name": contact.name}}), 200
