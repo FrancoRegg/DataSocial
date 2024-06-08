2037d405a109e6fd0a8e8192de88987dd97abe0b
FrancoRegg <francoreggiardo97@gmail.com>
2024-06-08 18:49:03 +0200

    Agrego archivo de configuracion e inicializo la base de datos

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 from flask import Flask
 import os
+from config import Config
+from models import db
 
 app = Flask(__name__)
+app.config.from_object(Config)
+
+db.init_app(app)
 
 @app.route('/')
 def home():
